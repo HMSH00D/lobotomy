270e9c896541630da5faa1eee87085ad48d3dfea
rotlogix <rotlogix@gmail.com>
2016-02-28 18:06:42 -0500

    Adding commmands for scalpel

--- framework/brains/scalpel/commands/cmd.py
+++ framework/brains/scalpel/commands/cmd.py
@@ -6,3 +6,48 @@ class Run(Scalpel):
     def __int__(self):
 
         Scalpel.__init__(self)
+
+    @staticmethod
+    def do_packages(args):
+        """
+
+        :param args:
+        :return:
+        """
+        return
+
+    @staticmethod
+    def do_activities(args):
+        """
+
+        :param args:
+        :return:
+        """
+        return
+
+    @staticmethod
+    def do_receivers(args):
+        """
+
+        :param args:
+        :return:
+        """
+        return
+
+    @staticmethod
+    def do_providers(args):
+        """
+
+        :param args:
+        :return:
+        """
+        return
+
+    @staticmethod
+    def do_services(args):
+        """
+        
+        :param args:
+        :return:
+        """
+        return
